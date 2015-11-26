2a7e96911db750569cff3789e62e3e8020bed735
Yadiel Martinez <ymglez2015@gmail.com>
2015-11-26 09:37:36 -0500

    Modifing jsonpack_real test

--- test/test_jsonpack_real.cpp
+++ test/test_jsonpack_real.cpp
@@ -31,67 +31,36 @@ struct  TestReal
 	DEFINE_JSON_ATTRIBUTES(test_float, test_double)
 };
 
-
-BOOST_AUTO_TEST_CASE(pack_reals_maximum_finite_value)
+BOOST_AUTO_TEST_CASE(pack_unpack_reals_maximum_finite_value)
 {
-    TestReal jsonpack_real;
+    TestReal jsonpack_real, output;
+
     jsonpack_real.test_float = std::numeric_limits<float>::max();
 	jsonpack_real.test_double = std::numeric_limits<double>::max();
 
     char * pack_string = jsonpack_real.json_pack();
 
-    std::ostringstream formatter;
-    formatter << "{\"test_float\":" << std::setprecision(17) << std::numeric_limits<float>::max()
-        << ",\"test_double\":" << std::setprecision(17) << std::numeric_limits<double>::max() << "}";
+	output.json_unpack(pack_string , strlen(pack_string));
 
-	BOOST_CHECK_EQUAL(pack_string, formatter.str().c_str() );
+    BOOST_CHECK_EQUAL(output.test_float, std::numeric_limits<float>::max());
+	BOOST_CHECK_EQUAL(output.test_double, std::numeric_limits<double>::max());
 
-    free(pack_string);
+	free(pack_string);
 }
 
-BOOST_AUTO_TEST_CASE(unpack_reals_maximum_finite_value)
+BOOST_AUTO_TEST_CASE(pack_unpack_reals_min_finite_value)
 {
-    TestReal jsonpack_real;
+    TestReal jsonpack_real, output;
 
-    std::ostringstream formatter;
-    formatter << "{\"test_float\":" << std::setprecision(17) << std::numeric_limits<float>::max()
-        << ",\"test_double\":" << std::setprecision(17) << std::numeric_limits<double>::max() << "}";
-
-	jsonpack_real.json_unpack(formatter.str().c_str() , formatter.str().length() );
-
-    BOOST_CHECK_EQUAL(jsonpack_real.test_float, std::numeric_limits<float>::max());
-	BOOST_CHECK_EQUAL(jsonpack_real.test_double, std::numeric_limits<double>::max());
-}
-
-
-BOOST_AUTO_TEST_CASE(pack_reals_min_finite_value)
-{
-	TestReal jsonpack_real;
-	jsonpack_real.test_float = std::numeric_limits<float>::min();
+    jsonpack_real.test_float = std::numeric_limits<float>::min();
 	jsonpack_real.test_double = std::numeric_limits<double>::min();
-
 
     char * pack_string = jsonpack_real.json_pack();
 
-    std::ostringstream formatter;
-    formatter << "{\"test_float\":" << std::setprecision(17) << std::numeric_limits<float>::min()
-        << ",\"test_double\":" << std::setprecision(17) << std::numeric_limits<double>::min() << "}";
+	output.json_unpack(pack_string , strlen(pack_string));
 
-	BOOST_CHECK_EQUAL(pack_string, formatter.str().c_str() );
-
-    free(pack_string);
-}
-
-BOOST_AUTO_TEST_CASE(unpack_reals_min_finite_value)
-{
-    TestReal jsonpack_real;
-
-    std::ostringstream formatter;
-    formatter << "{\"test_float\":" << std::setprecision(17) << std::numeric_limits<float>::min()
-        << ",\"test_double\":" << std::setprecision(17) << std::numeric_limits<double>::min() << "}";
-
-	jsonpack_real.json_unpack(formatter.str().c_str(), formatter.str().length() );
-
-	BOOST_CHECK_EQUAL(jsonpack_real.test_float, std::numeric_limits<float>::min());
-	BOOST_CHECK_EQUAL(jsonpack_real.test_double, std::numeric_limits<double>::min());
+    BOOST_CHECK_EQUAL(output.test_float, std::numeric_limits<float>::min());
+	BOOST_CHECK_EQUAL(output.test_double, std::numeric_limits<double>::min());
+	
+	free(pack_string);
 }
